64661188ecb7479d3804681b7c1bf373b4556850
Gian <gian.defaria@hotmail.com>
2022-11-10 23:49:10 -0300

    Selecionei os elementos html pela classe. Após, criei uma variável e funções que adicionarão os dados de acordo com o array indicado no for, adicionando texto respectivo no html

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 console.log (obj[0].title)
 
+const boxWork = document.querySelector(".work");
+const title = document.querySelectorAll(".title");
+const currentHour = document.querySelectorAll(".current--hour");
+const lastWeek = document.querySelectorAll(".last--week");
+
+var selector = 'weekly';
+
+let i = 0;
+
+
+for (let i = 0; i < title.length; i++) {
+    console.log(i);
+    title[i].innerText = obj[i].title;
+    currentHour[i].innerText = obj[i].timeframes[selector].current + "hrs";
+    lastWeek[i].innerText = "Last Week - " + obj[i].timeframes[selector].previous;
+    
+}
+
